e8fe883589227fb6dae257a68feac860d797d836
btq0002 <31081877+btq0002@users.noreply.github.com>
2018-09-25 18:35:36 -0400

    Lexer Upload
    Bare Bones Lexer Class

---
+++ CS310TeamSigma/src/team/Lexer.java
@@ -0,0 +1,17 @@
+package team;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class Lexer {
+
+	public void initialize(String sentence) {
+		input = new StringReader(sentence);
+	}
+	
+	private StringReader input;
+	
+	public void lex() {
+		
+	}
+}
